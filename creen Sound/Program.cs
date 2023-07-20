f2859850dd41560b9095d790a8189d3e19d6cc56
Marco Antonio <marcotonho38@gmail.com>
2023-07-20 20:49:38 -0300

    FEAT : Add Menu e Titulo

--- creen Sound/Program.cs
+++ creen Sound/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string mensagemDeboasVindas = " Boas vindas ao Screen Sound";
+
+void ExibirMensagemDeBoasVinda()
+{
+Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+Console.WriteLine(mensagemDeboasVindas);
+}
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    String opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:  Console.WriteLine("Você e escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+            case 2: Console.WriteLine("Você e escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+            case 3: Console.WriteLine("Você e escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+            case 4: Console.WriteLine("Você e escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+            case -1: Console.WriteLine("Tchau Tchau :)");
+            break;
+            default: Console.WriteLine("Opção Invalida");
+            break;
+
+    }
+}
+ExibirMensagemDeBoasVinda();
+ExibirOpcoesDoMenu();
